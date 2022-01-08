71c21228fe877040de88131f73681d8346fd6ea2
vinicius <viniscoelho@gmail.com>
2022-01-08 18:46:01 -0300

    inicio processo

---
+++ src/main/java/com/vcoelho/geral/ServicosGeraisApplication.java
@@ -0,0 +1,13 @@
+package com.vcoelho.geral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicosGeraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicosGeraisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcoelho/geral/ServicosGeraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcoelho.geral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosGeraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
